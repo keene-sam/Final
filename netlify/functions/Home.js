255c558b97b6531ffce27e6884c088846dcff314
Sammy Keene <sammy.keene@kellogg.northwestern.edu>
2021-05-30 14:37:34 -0500

    New release nft js

---
+++ netlify/functions/Home.js
@@ -0,0 +1,43 @@
+// allows us to use firebase
+let firebase = require(`./firebase`)
+
+// /.netlify/functions/Home
+exports.handler = async function(event) {
+  // define an empty Array to hold the return value from our lambda
+  let returnValue = []
+
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // perform a query against firestore for all nfts, wait for it to return, store in memory
+
+  let nftQuery = await db.collection(`nftCollection`).orderBy(`timestampNftCreated`, `desc`).get()
+
+  // retrieve the documents from the query
+  let nfts = nftQuery.docs
+
+  // loop through the nft documents
+  for (let nftIndex=0; nftIndex < 1; nftIndex++) {
+    // get the id from the document
+    let nftId = nfts[nftIndex].id
+
+    // get the data from the document
+    let nftData = nfts[nftIndex].data()
+
+    // create an Object to be added to the return value of our lambda
+    let releaseObject = {
+      id: nftId,
+      imageUrl: nftData.urlNftFile,
+      description: nftData.description,
+    }
+
+    // add the Object to the return value
+    returnValue.push(releaseObject)
+  }
+
+  // return value of our lambda
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
+}
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+    apiKey: "AIzaSyBvKFBcV-fCeZb51JqS0rSq3_N_h4sQbSw",
+    authDomain: "nft-marketplace-70a38.firebaseapp.com",
+    projectId: "nft-marketplace-70a38",
+    storageBucket: "nft-marketplace-70a38.appspot.com",
+    messagingSenderId: "425588971380",
+    appId: "1:425588971380:web:1b8e567fda1d39e3853e21",
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
