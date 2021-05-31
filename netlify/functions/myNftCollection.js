75e1beb76182e762da3c1dcde286dcdf0357f16a
andrea <andrea.punchera@kellogg.northwestern.edu>
2021-05-31 10:37:21 -0500

    Add myCollection and add min function to home.js

--- netlify/functions/myNftCollection.js
+++ netlify/functions/myNftCollection.js
@@ -4,30 +4,30 @@ let firebase = require(`./firebase`)
 // /.netlify/functions/Home
 exports.handler = async function(event) {
   // define an empty Array to hold the return value from our lambda
-  let returnValue = []
+  let returnMyNfts = []
 
   // establish a connection to firebase in memory
   let db = firebase.firestore()
+  let userId = event.queryStringParameters.userId
+
 
   // perform a query against firestore for all nfts, wait for it to return, store in memory
 
-  let nftQuery = await db.collection(`nftCollection`).where(`forSale`,`==`,true).get()
+  let myNftQuery = await db.collection(`nftCollection`).where('userId', '==', userId ).get()
 
   // retrieve the documents from the query
-  let nfts = nftQuery.docs
+  let myNfts = myNftQuery.docs
 
-  nfts.sort()
-
-  //define minimum of releases to show
-  let minNftView = Math.min(nfts.length, 5)
+  myNfts.sort()
+  console.log(myNfts.length)
   
   // loop through the nft documents
-  for (let nftIndex=0; nftIndex < minNftView; nftIndex++) {
+  for (let nftIndex=0; nftIndex < myNfts.length; nftIndex++) {
     // get the id from the document
-    let nftId = nfts[nftIndex].id
+    let nftId = myNfts[nftIndex].id
 
     // get the data from the document
-    let nftData = nfts[nftIndex].data()
+    let nftData = myNfts[nftIndex].data()
 
     // create an Object to be added to the return value of our lambda
     let releaseObject = {
@@ -37,12 +37,14 @@ exports.handler = async function(event) {
     }
 
     // add the Object to the return value
-    returnValue.push(releaseObject)
+    returnMyNfts.push(releaseObject)
   }
 
   // return value of our lambda
   return {
     statusCode: 200,
-    body: JSON.stringify(returnValue)
+    body: JSON.stringify(returnMyNfts)
   }
 }
+
+
