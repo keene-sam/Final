a3ca2c556de64d2fe3b85e3f149a2f550c655d94
Alaa <alaa.aldeen@kellogg.northwestern.edu>
2021-05-31 22:53:09 -0500

    account pages update

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyDpTEYLGJ5ENqmLRswui9heynZepmWlAeM",
+  authDomain: "finalkieiproject.firebaseapp.com",
+  projectId: "finalkieiproject",
+  storageBucket: "finalkieiproject.appspot.com",
+  messagingSenderId: "571182565488",
+  appId: "1:571182565488:web:a2808fb1bc8b4c9bc20cb5"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
---
+++ netlify/functions/nfts.js
@@ -0,0 +1,86 @@
+// allows us to use firebase
+let firebase = require(`./firebase`)
+
+// /.netlify/functions/Home
+exports.handler = async function(event) {
+  // define an empty Array to hold the return value from our lambda
+  let returnValue = []
+
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // perform a query against firestore for all nfts, wait for it to return, store in memory
+
+  let nftQuery = await db.collection(`nftCollection`).where(`forSale`,`==`,true).get()
+
+  // retrieve the documents from the query
+  let nfts = nftQuery.docs
+
+  
+  // loop through the nft documents
+  for (let nftIndex=0; nftIndex < nfts.length; nftIndex++) {
+
+    // get the id from the document
+    let nftId = nfts[nftIndex].id
+
+    // get the data from the document
+    let nftData = nfts[nftIndex].data()
+
+    // create an Object to be added to the return value of our lambda
+    let nftObject = {
+      id: nftId,
+      name: nftData.name,
+      ownerId: nftData.ownerId,
+      category: nftData.category,
+      description: nftData.description,
+      forSale: nftData.forSale,
+      urlNft: nftData.urlNft,
+      price: nftData.price,
+      timestampNftCreated: nftData.timestampNftCreated
+    }
+
+
+    // add the Object to the return value
+    returnValue.push(nftObject)
+
+    //sort the items in the array by the timestamp of each element
+    returnValue.sort(function(x, y){
+    return x.timestampNftCreated - y.timestampNftCreated;})  
+
+    }
+
+ 
+
+
+  // return value of our lambda
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
+
+}
+
+
+    // // get the comments for this post, wait for it to return, store in memory
+    // let commentsQuery = await db.collection(`comments`).where(`postId`, `==`, postId).get()
+
+    // // get the documents from the query
+    // let comments = commentsQuery.docs
+
+    // // loop through the comment documents
+    // for (let commentIndex=0; commentIndex < comments.length; commentIndex++) {
+    //   // get the id from the comment document
+    //   let commentId = comments[commentIndex].id
+
+    //   // get the data from the comment document
+    //   let commentData = comments[commentIndex].data()
+
+    //   // create an Object to be added to the comments Array of the post
+    //   let commentObject = {
+    //     id: commentId,
+    //     userName: commentData.userName,
+    //     body: commentData.body
+    //   }
+
+    //   // add the Object to the post
+    //   postObject.comments.push(commentObject)
