ff317b5105be07ac0c0c435362d0496822015738
Alaa <alaa.aldeen@kellogg.northwestern.edu>
2021-06-01 10:09:36 -0500

    merging

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,7 +2,6 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyDpTEYLGJ5ENqmLRswui9heynZepmWlAeM",
   authDomain: "finalkieiproject.firebaseapp.com",
   projectId: "finalkieiproject",
@@ -10,15 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "571182565488",
   appId: "1:571182565488:web:a2808fb1bc8b4c9bc20cb5"
 }
-=======
-    apiKey: "AIzaSyBvKFBcV-fCeZb51JqS0rSq3_N_h4sQbSw",
-    authDomain: "nft-marketplace-70a38.firebaseapp.com",
-    projectId: "nft-marketplace-70a38",
-    storageBucket: "nft-marketplace-70a38.appspot.com",
-    messagingSenderId: "425588971380",
-    appId: "1:425588971380:web:1b8e567fda1d39e3853e21",
-} // replace
->>>>>>> 75e1beb76182e762da3c1dcde286dcdf0357f16a
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
