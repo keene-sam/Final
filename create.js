dcfc41236d665ac9a46a5a4426b4d136978ca070
Alaa <alaa.aldeen@kellogg.northwestern.edu>
2021-06-04 15:55:37 -0500

    adding creating  a new NFT functionality

---
+++ create.js
@@ -0,0 +1,82 @@
+// standard event listener for Firebase auth... use instead of DOMContentLoaded
+firebase.auth().onAuthStateChanged(async function(user) {
+
+    // check to see if user is logged-in (i.e. user exists)
+    if (user) {
+      // write the user Object to the JavaScript console
+      console.log(user)
+  
+      // Build the markup for the sign-out button and set the HTML in the header
+      document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+        <button class="text-pink-500 underline sign-out">Sign Out</button>
+      `
+  
+      // get a reference to the sign out button
+      let signOutButton = document.querySelector(`.sign-out`)
+  
+      // handle the sign out button click
+      signOutButton.addEventListener(`click`, function(event) {
+        // sign out of firebase authentication
+        firebase.auth().signOut()
+  
+        // redirect to the home page
+        document.location.href = `index.html`
+      })
+  
+      let userId = user.uid
+  console.log(userId)
+      // Build the URL for our nft API
+      let url = `/.netlify/functions/nfts`
+  
+    // get a reference to the "Create" button
+    let createButton = document.querySelector(`#create-button`)
+
+    // handle the clicking of the "Create" button
+    createButton.addEventListener(`click`, async function(event) {
+      // prevent the default behavior (submitting the form)
+      event.preventDefault()
+console.log(`is it working`)
+      // get a reference to the input holding the URL, name, description and category
+      let nftUrlInput = document.querySelector(`#nft-url`)
+      let nftNameInput = document.querySelector(`#nft-name`)
+      let nftDescriptionInput = document.querySelector(`#nft-description`)
+      let nftCategoryInput = document.querySelector(`#nft-category`)
+console.log(nftCategoryInput)
+
+      // store the user-inputted image URL in memory
+      let nftUrl = nftUrlInput.value
+      let nftName = nftNameInput.value
+      let nftDescription = nftDescriptionInput.value
+      let nftCategory = nftCategoryInput.value
+
+
+      // create the URL for our "create post" lambda function
+      let url = `/.netlify/functions/create_nft?userId=${userId}&nftUrl=${nftUrl}&nftName=${nftName}&nftDescription=${nftDescription}&nftCategory=${nftCategory}`
+
+      // fetch the URL, wait for the response, store the response in memory
+      let response = await fetch(url)
+
+      // refresh the page
+      location.reload()
+    })
+
+    } else {
+      // user is not logged-in, so show login
+      // Initializes FirebaseUI Auth
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+  
+      // FirebaseUI configuration
+      let authUIConfig = {
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        signInSuccessUrl: `index.html` // where to go after we're done signing up/in
+      }
+  
+      // Starts FirebaseUI Auth
+      ui.start(`.sign-in-or-sign-out`, authUIConfig)
+    }
+  })
+  
+  
+  
---
+++ netlify/functions/create_nft.js
@@ -0,0 +1,37 @@
+// Goal: Provide a function to create a new post in Firebase
+
+// allows us to use firebase
+let firebase = require(`./firebase`)
+
+// /.netlify/functions/create_post?userName=Brian&imageUrl=https://images.unsplash.com/...
+exports.handler = async function(event) {
+
+  // get the two querystring parameters and store in memory
+  
+  let ownerId = event.queryStringParameters.userId
+  let name = event.queryStringParameters.nftName
+  let category = event.queryStringParameters.nftCategory
+  let description = event.queryStringParameters.nftDescription
+  let urlNft = event.queryStringParameters.nftUrl
+
+console.log(`The description is ${description}`)
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // create a new post, wait for it to return
+  await db.collection('nftCollection').add({
+    name: name,
+    ownerId: ownerId,
+    category: category,
+    description: description,   
+    forSale: false,
+    urlNft: urlNft,
+    price: 0,
+    timestampNftCreated: firebase.firestore.FieldValue.serverTimestamp()
+  })
+
+  return {
+    statusCode: 200
+  }
+}
+
